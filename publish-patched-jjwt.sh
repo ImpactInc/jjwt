618893274f9b137ad62804ee0cee374343f293a5
Johan Roets <johan@impact.com>
2024-02-16 08:49:31 +0200

    DEVE-142: deploy other jars

--- publish-patched-jjwt.sh
+++ publish-patched-jjwt.sh
@@ -1,4 +1,4 @@
-./mvnw install -Dmaven.test.skip=true
+#./mvnw install -Dmaven.test.skip=true
 
 ./mvnw deploy:deploy-file -Durl=http://ir-nexus-gcp.inf-impact.net:8081/repository/thirdparty \
                        -DrepositoryId=thirdparty \
@@ -6,6 +6,26 @@
                        -DpomFile=impl/pom.xml \
                        -DgroupId=io.jsonwebtoken \
                        -DartifactId=jjwt-impl \
+                       -Dversion=0.12.5-IMPACT-PATCHED \
+                       -Dpackaging=jar \
+                       -DgeneratePom=false
+
+./mvnw deploy:deploy-file -Durl=http://ir-nexus-gcp.inf-impact.net:8081/repository/thirdparty \
+                       -DrepositoryId=thirdparty \
+                       -Dfile=api/target/jjwt-api-0.12.5-IMPACT-PATCHED.jar \
+                       -DpomFile=api/pom.xml \
+                       -DgroupId=io.jsonwebtoken \
+                       -DartifactId=jjwt-api \
+                       -Dversion=0.12.5-IMPACT-PATCHED \
+                       -Dpackaging=jar \
+                       -DgeneratePom=false
+
+./mvnw deploy:deploy-file -Durl=http://ir-nexus-gcp.inf-impact.net:8081/repository/thirdparty \
+                       -DrepositoryId=thirdparty \
+                       -Dfile=tdjar/target/jjwt-0.12.5-IMPACT-PATCHED.jar \
+                       -DpomFile=pom.xml \
+                       -DgroupId=io.jsonwebtoken \
+                       -DartifactId=jjwt \
                        -Dversion=0.12.5-IMPACT-PATCHED \
                        -Dpackaging=jar \
                        -DgeneratePom=false
